a482e2c1bba721082beacaeabe440390de912c4f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-17 20:06:04 +0000

    the implementation of task 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- An SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT 
+  origin, 
+  nb_fans, 
+  RANK() OVER (ORDER BY nb_fans DESC) as rank
+FROM 
+  metal_bands;
+  
