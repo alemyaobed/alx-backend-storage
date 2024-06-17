e1add6389bee95e9a0c08cbbbf6eb3e7e65c6234
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-17 20:16:01 +0000

    some minor changes

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,10 @@
 -- An SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SET @rank=0;
-SELECT 
-  origin, 
-  nb_fans, 
-  @rank:=@rank+1 as rank
-FROM 
-  metal_bands
-ORDER BY 
-  nb_fans DESC;
+SELECT
+    origin,
+    fans AS nb_fans,
+    RANK() OVER (
+        ORDER BY
+            nb_fans DESC
+    ) as rank
+FROM
+    metal_bands;
