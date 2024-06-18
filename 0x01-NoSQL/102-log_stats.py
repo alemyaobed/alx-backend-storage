d2d880a46751cbde43a948209e2d335802a634a6
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-18 19:05:00 +0000

    some minor changes

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -23,25 +23,21 @@ def log_stats():
     # Number of documents with method=GET and path=/status
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
-        )
+    )
     print(f"{status_check} status check")
 
-    def get_top_ips(mongo_collection):
-        pipeline = [
-            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-            {"$sort": {"count": -1}},
-            {"$limit": 10}
-        ]
-        top_ips = {
-            ip["_id"]: ip["count"] for ip in mongo_collection.
-            aggregate(pipeline)}
-        return top_ips
-
-    # Top 10 IPs
-    top_ips = get_top_ips(collection)
+    # Top 10 IPs with the highest count
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = list(collection.aggregate(pipeline))
     print("IPs:")
-    for ip, count in top_ips.items():
-        print(f"    {ip}: {count}")
+    for ip_info in top_ips:
+        ip = ip_info["_id"]
+        count = ip_info["count"]
+        print(f"\t{ip}: {count}")
 
 
 if __name__ == "__main__":
